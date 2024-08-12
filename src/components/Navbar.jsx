ea0c92c3831c133fb0ec991c7f303feadb986081
ShubhamKarala <shubhamkarala@gmail.com>
2024-08-12 12:22:43 +0530

    initial

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import logo from "../assets/kevinRushLogo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+function Navbar() {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-10" src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://www.linkedin.com/in/shubhamkarala/">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/ShubhamKarala">
+          <FaGithub />
+        </a>
+        <a href="https://x.com/ShubhamKarala">
+          <FaTwitter />
+        </a>
+        <a href="https://www.instagram.com/shubhamkarala/">
+          <FaInstagram />
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
